08e67ee621e7c0ca6736c45b8f183ba9de32db1b
Zainds <vasiliyjoskiy@gmil.com>
2020-10-29 19:12:41 +0700

    добавил кнопку, обработку

--- app/src/main/java/com/example/startandroid/MainActivity.kt
+++ app/src/main/java/com/example/startandroid/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.startandroid
 
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myBtn = findViewById<Button>(R.id.myBtn)
+        var myTxt = findViewById<TextView>(R.id.myTextView)
+        myBtn.setOnClickListener {
+            Toast.makeText(this, "Нет у тебя силы воли...", Toast.LENGTH_LONG).show();
+            myTxt.text = "Зачем нажал?"
+        }
     }
 }
